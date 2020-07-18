831506072075042af9d214a40096d10a2bb4994d
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2020-07-18 19:35:32 -0300

    implements appbar

--- lib/pages/Home/home_page.dart
+++ lib/pages/Home/home_page.dart
@@ -1,15 +1,42 @@
+import 'package:dark/utils/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: SystemUiOverlayStyle(
-        statusBarColor: Colors.white,
-        statusBarIconBrightness: Brightness.dark,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Row(
+            children: <Widget>[
+              Image.asset(
+                AppConstants.triquetra,
+                height: 33,
+                width: 33,
+              )
+            ],
+          ),
+        ),
+        title: Text(
+          'D A R K',
+          style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Mulish',
+              fontWeight: FontWeight.w200,
+              wordSpacing: 3),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            color: Colors.black,
+            onPressed: () {},
+          )
+        ],
       ),
-      child: Scaffold(),
     );
   }
 }
