ad229b2e07b287ae24bb7392f58e98ce98f997b8
andersonalexdurante <anderson.alexdurante@gmail.com>
2020-07-19 13:50:47 -0300

    refactoring home_page

--- lib/pages/Home/home_page.dart
+++ lib/pages/Home/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:dark/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'widgets/app_bar.dart';
+import 'widgets/home_body.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,99 +10,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final _width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 16.0),
-          child: Row(
-            children: <Widget>[
-              Image.asset(
-                AppConstants.triquetra,
-                height: 33,
-                width: 33,
-              )
-            ],
-          ),
-        ),
-        title: Text(
-          'D A R K',
-          style: TextStyle(
-              color: Colors.black,
-              fontFamily: 'Mulish',
-              fontWeight: FontWeight.w200,
-              wordSpacing: 3),
-        ),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            color: Colors.black,
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(50),
-                  bottomRight: Radius.circular(50)),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40),
-                  bottomRight: Radius.circular(40)),
-              color: Colors.white,
-            ),
-            height: 100,
-            width: _width,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 16.0, bottom: 10.0),
-                  child: Text('H E L L O',
-                      style: TextStyle(
-                          fontFamily: 'Mulish',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 25,
-                          letterSpacing: 3)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 16),
-                  child: Text('T R A V E L E R',
-                      style: TextStyle(
-                          fontFamily: 'Mulish',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 25,
-                          letterSpacing: 3)),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.date_range), title: Text('When')),
-          BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Who')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person), title: Text('Profile')),
-        ],
-        type: BottomNavigationBarType.fixed,
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Colors.black,
-      ),
+      appBar: AppBarDark(),
+      body: HomeBody(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.all_inclusive),
         onPressed: () {},
